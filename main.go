f124b91d1b7b71fbbe388da483f9b447f6922799
mitsu3206 <kohshelltoh3206@gmail.com>
2025-08-23 22:07:22 +0900

    feat: Implement calculation and database storage

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	gmysql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type CalculationResult struct {
+	gorm.Model
+	FloatValue float64
+}
+
+var DB *gorm.DB
 
 func main() {
-	fmt.Println("Hello, World!")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	conf := &mysql.Config{
+		User:      os.Getenv("DB_USER"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
+		DBName:    os.Getenv("DB_NAME"),
+		Loc:       jst,
+		ParseTime: true,
+	}
+	db, err := gorm.Open(gmysql.Open(conf.FormatDSN()), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	DB = db
+
+	if err := db.AutoMigrate(&CalculationResult{}); err != nil {
+		panic("failed to migrate database")
+	}
+	calcFloat()
+}
+
+func calcFloat() {
+	var f float64
+	for i := 0; i < 10; i++ {
+		f += 0.1
+	}
+	fmt.Println(f)
+	DB.Create(&CalculationResult{FloatValue: f})
 }
